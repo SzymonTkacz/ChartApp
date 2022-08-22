660d10f1730a62dc1bfcadfbb9336bab3dcc0a59
Szymon Tkacz <szymon.tkacz@apagroup.pl>
2022-08-22 15:39:32 +0200

    quick fix

--- app/src/app/components/table/table.component.ts
+++ app/src/app/components/table/table.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user-model';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TableComponent implements OnInit {
 
